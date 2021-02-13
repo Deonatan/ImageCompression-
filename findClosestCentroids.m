4ab623b98127491447e554e24a99b57fedbd59bb
Deonatan Dinata <69316014+Deonatan@users.noreply.github.com>
2021-02-14 00:20:31 +0800

    Find the closest centroids

---
+++ findClosestCentroids.m
@@ -0,0 +1,17 @@
+function idx = findClosestCentroids(X, centroids) 
+  K = size(centroids , 1); 
+  idx = zeros(size(X,1),1); 
+  
+  for i=1:size(X,1)
+  minDist = -1 ; 
+  for j = 1:K 
+    mat = X(i, :) - centroids(j, :) ;
+    dist = mat*mat';
+    if idx(i) == 0 || dist < minDist 
+      minDist = dist; 
+      idx(i) = j;
+    endif
+    endfor
+  endfor
+
+end 
